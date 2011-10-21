669a566e27aaa4a009b46cf8cc4598936b4e1f70
Travis Jeffery <travisj@nulogy.com>
2011-10-20 20:56:25 -0500

    Clojure/Emacs stuff.

---
+++ .emacs.d/el-get/.loaddefs.el
@@ -0,0 +1,32 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (textmate-mode) "textmate/textmate" "textmate/textmate.el"
+;;;;;;  (20105 4940))
+;;; Generated autoloads from textmate/textmate.el
+
+(defvar textmate-mode nil "\
+Non-nil if Textmate mode is enabled.
+See the command `textmate-mode' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `textmate-mode'.")
+
+(custom-autoload 'textmate-mode "textmate/textmate" nil)
+
+(autoload 'textmate-mode "textmate/textmate" "\
+TextMate Emulation Minor Mode
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
---
+++ .emacs.d/init.el
@@ -0,0 +1,75 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(defun ido-goto-symbol (&optional symbol-list)
+  "Refresh imenu and jump to a place in the buffer using Ido."
+  (interactive)
+  (unless (featurep 'imenu)
+    (require 'imenu nil t))
+  (cond
+   ((not symbol-list)
+    (let ((ido-mode ido-mode)
+          (ido-enable-flex-matching
+           (if (boundp 'ido-enable-flex-matching)
+               ido-enable-flex-matching t))
+          name-and-pos symbol-names position)
+      (unless ido-mode
+        (ido-mode 1)
+        (setq ido-enable-flex-matching t))
+      (while (progn
+               (imenu--cleanup)
+               (setq imenu--index-alist nil)
+               (ido-goto-symbol (imenu--make-index-alist))
+               (setq selected-symbol
+                     (ido-completing-read "Symbol? " symbol-names))
+               (string= (car imenu--rescan-item) selected-symbol)))
+      (unless (and (boundp 'mark-active) mark-active)
+        (push-mark nil t nil))
+      (setq position (cdr (assoc selected-symbol name-and-pos)))
+      (cond
+       ((overlayp position)
+        (goto-char (overlay-start position)))
+       (t
+        (goto-char position)))))
+   ((listp symbol-list)
+    (dolist (symbol symbol-list)
+      (let (name position)
+        (cond
+         ((and (listp symbol) (imenu--subalist-p symbol))
+          (ido-goto-symbol symbol))
+         ((listp symbol)
+          (setq name (car symbol))
+          (setq position (cdr symbol)))
+         ((stringp symbol)
+          (setq name symbol)
+          (setq position
+                (get-text-property 1 'org-imenu-marker symbol))))
+        (unless (or (null position) (null name)
+                    (string= (car imenu--rescan-item) name))
+          (add-to-list 'symbol-names name)
+          (add-to-list 'name-and-pos (cons name position))))))))
+
+(global-set-key [(super shift t)] 'ido-goto-symbol)
+                                        ; (global-set-key (kbd "A-T") 'ido-goto-symbol)
+
+
+(global-linum-mode)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(set-frame-font "Inconsolata-14")
