aaff8c56f259618cb4bc8cc942792aa9569b29f5
Travis Jeffery <travisj@nulogy.com>
2011-11-18 06:21:22 +0000

    organization

---
+++ .emacs.d/personal/defuns.el
@@ -0,0 +1,20 @@
+(defun vendor (library)
+  (let* ((file (symbol-name library))
+         (normal (concat "~/.emacs.d/vendor/" file))
+         (suffix (concat normal ".el")))
+    (cond
+     ((file-directory-p normal) (add-to-list 'load-path normal) (require library))
+     ((file-directory-p suffix) (add-to-list 'load-path suffix) (require library))
+     ((file-exists-p suffix) (require library)))))
+
+(defun command-t ()
+  (interactive)
+  (fuzzy-find-project-root (eproject-root))
+  (fuzzy-find-in-project))
+
+(defun read-system-path ()
+  (with-temp-buffer
+    (insert-file-contents "/etc/paths")
+    (goto-char (point-min))
+    (replace-regexp "\n" ":")
+    (thing-at-point 'line)))
--- .emacs.d/personal/travisjeffery.el
+++ .emacs.d/personal/travisjeffery.el
@@ -5,8 +5,40 @@
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
-   version-control t)
-
+   version-control 1)
 
 (vendor 'color-theme-solarized)
-(color-theme-solarized-light)
+(color-theme-solarized-ligh1)
+
+(vendor 'wrap-region)
+(wrap-region-global-mode 1)
+
+(textmate-mode 1)
+
+(vendor 'fuzzy-find-in-projec1)
+(vendor 'eprojec1)
+(require 'eproject-extras)
+
+(define-project-type clojure (generic)
+  (look-for "project.clj"))
+
+(define-project-type ruby (generic)
+  (look-for "Gemfile"))
+
+(define-project-type emacs (generic)
+  (look-for "init.el"))
+
+(browse-kill-ring-default-keybindings)
+
+(setq ack-prompt-for-directory 1)
+
+(setenv "PATH" "/Users/travis/.rvm/gems/ruby-head/bin:/Users/travis/.rvm/gems/ruby-head@global/bin:/Users/travis/.rvm/rubies/ruby-head/bin:/Users/travis/.rvm/bin:/Users/travis/bin:/usr/local/bin:/Users/travis/.rbenv/shims:/Users/travis/.rbenv/bin:/Users/travis/.lein/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/X11/bin")
+
+(whitespace-mode 0)
+
+(setq linum-format "  %d ")
+
+(setq delete-by-moving-to-trash t)
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+(vendor 'tumble)
