64b63aafa6b1889aaab89b40d08d58a1f8ed9402
Travis Jeffery <travisj@nulogy.com>
2011-11-19 20:05:42 -0500

    update prelude

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(prelude-disable-arrow-navigation nil)
  '(prelude-use-default-prelude-theme nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- .emacs.d/personal/travisjeffery.el
+++ .emacs.d/personal/travisjeffery.el
@@ -5,7 +5,21 @@
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
-   version-control 1)
+   version-control t)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message file)
+      (delete-file file))))
 
 (vendor 'color-theme-solarized)
 (color-theme-solarized-light)
@@ -61,6 +75,9 @@
 
 (whitespace-mode 0)
 
+(load "vimgolf")
+
+(global-linum-mode 1)
 (setq linum-format "  %d ")
 
 (setq delete-by-moving-to-trash t)
