316b48873a4d000a86a8fe01b81bcbbb18f67bed
Travis Jeffery <travisj@nulogy.com>
2011-11-22 03:59:40 -0500

    vertical ido

--- .emacs.d/personal/travisjeffery.el
+++ .emacs.d/personal/travisjeffery.el
@@ -48,11 +48,15 @@
 (vendor 'pomodoro)
 (load "vimgolf")
 
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
+
 (require 're-builder)
 (setq reb-re-syntax 'string)
 
-(setq cua-enable-cua-keys nil)           
-(cua-mode t)                             
+(setq cua-enable-cua-keys nil)
+(cua-mode t)
 
 (setq
  backup-by-copying t
