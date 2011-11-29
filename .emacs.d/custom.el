254799c1f2db073418654d8ebf1fbf2c2e19b511
Travis Jeffery <travisj@nulogy.com>
2011-11-29 00:17:21 -0500

    a usable eshell-prompt face

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("5600dc0bb4a2b72a613175da54edb4ad770105aa" default))))
+ '(custom-safe-themes (quote ("7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(eshell-prompt ((t (:foreground "#2075c7" :weight bold)))))
