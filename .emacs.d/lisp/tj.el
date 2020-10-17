34ed24817e858d99ab9e92bb6e90c027bd58985f
Travis Jeffery <tj@travisjeffery.com>
2020-10-17 16:52:54 -0400

    emacs: add tj-fill-paragraph

--- .emacs.d/lisp/tj.el
+++ .emacs.d/lisp/tj.el
@@ -54,6 +54,8 @@
 ;; the blinking cursor is nothing, but an annoyance
 (blink-cursor-mode -1)
 
+(setq sentence-end-double-space nil)
+
 ;; disable the annoying bell ring
 (setq ring-bell-function 'ignore)
 
@@ -685,6 +687,32 @@ will be killed."
 (defadvice backward-kill-word (around fix activate)
   (cl-flet ((kill-region (b e) (delete-region b e)))
     ad-do-it))
+
+(defun tj-fill-paragraph (&optional P)
+  "When called with prefix argument call `fill-paragraph'.
+Otherwise split the current paragraph into one sentence per line."
+  (interactive "P")
+  (if (not P)
+      (save-excursion 
+        (let ((fill-column 12345678)) ;; relies on dynamic binding
+          (fill-paragraph) ;; this will not work correctly if the paragraph is
+                           ;; longer than 12345678 characters (in which case the
+                           ;; file must be at least 12MB long. This is unlikely.)
+          (let ((end (save-excursion
+                       (forward-paragraph 1)
+                       (backward-sentence)
+                       (point-marker))))  ;; remember where to stop
+            (beginning-of-line)
+            (while (progn (forward-sentence)
+                          (<= (point) (marker-position end)))
+              (just-one-space) ;; leaves only one space, point is after it
+              (delete-char -1) ;; delete the space
+              (newline)        ;; and insert a newline
+              ))))
+    ;; otherwise do ordinary fill paragraph
+    (fill-paragraph P)))
+
+(global-set-key (kbd "M-q") 'tj-fill-paragraph)
 
 (defun tj-apply-function-to-region (fn)   
   (interactive "XFunction to apply to region: ")   
